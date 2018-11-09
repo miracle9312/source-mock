c5aabb41bde9cd8f9a8d9e8215daf8a4a20f3456
shaoxuezheng <shaoxuezheng@didichuxing.com>
2018-11-09 10:28:18 +0800

    module注册

--- src/vue/vuex/src/modules/module-collection.js
+++ src/vue/vuex/src/modules/module-collection.js
@@ -1,3 +1,4 @@
+import { forEachValue } from "../util";
 import Module from "./module";
 
 export default class ModuleCollection {
@@ -6,10 +7,27 @@ export default class ModuleCollection {
     this.register([], rawRootModule, false);
   }
 
-  // 1.todo runtime的作用？
-  register (path, rawRootModule, runtime) {
-    const module = new Module(rawRootModule, runtime);
+  // 根据路径获取模块，从root开始搜索
+  get (path) {
+    return path.reduce((module, key) => module.getChild(key), this.root);
+  }
 
-    this.root = module;
+  // 1.todo runtime的作用？
+  register (path, rawModule, runtime = true) {
+    // 生成module
+    const newModule = new Module(rawModule, runtime);
+    if (path.length === 0) { // 根模块，注册在root上
+      this.root = newModule;
+    } else { // 非根模块，获取父模块，挂载
+      const parent = this.get(path.slice(0, -1));
+      parent.addChild(path[path.length - 1], newModule);
+    }
+
+    // 模块上是否含有子模块，有则注册子模块
+    if (rawModule.modules) {
+      forEachValue(rawModule.modules, (newRawModule, key) => {
+        this.register(path.concat(key), newRawModule, runtime);
+      });
+    }
   }
 }
--- src/vue/vuex/src/modules/module.js
+++ src/vue/vuex/src/modules/module.js
@@ -6,7 +6,17 @@ export default class Module {
 
     this.runtime = runtime;// 1.todo:runtime的作用是啥
     this._rawModule = rawModule;
+    this._children = Object.create(null);
+    // 此处state可以为函数，主要用于多个模块同时引用一个state时，防止数据污染
     this.state = typeof rawState === "function" ? rawState() : rawState;
+  }
+
+  getChild (key) {
+    return this._children[key];
+  }
+
+  addChild (key, module) {
+    this._children[key] = module;
   }
 
   // 遍历mumation,执行函数
